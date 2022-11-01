1644d7798991c8334d9c36f13acf4d53602be7d9
Karol Kołtun <karol.koltun@woodpecker.co>
2022-11-01 12:24:33 +0100

    Add predicate locking and NOWAIT behavior

--- src/test/java/com/concurrency/PessimisticLockingExamples.java
+++ src/test/java/com/concurrency/PessimisticLockingExamples.java
@@ -5,10 +5,13 @@ import org.hibernate.FlushMode;
 import org.hibernate.LockMode;
 import org.hibernate.LockOptions;
 import org.hibernate.Session;
+import org.hibernate.cfg.AvailableSettings;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.LockModeType;
+import java.util.Collections;
 import java.util.function.Consumer;
 
 public class PessimisticLockingExamples extends HibernateTest {
@@ -47,6 +50,34 @@ public class PessimisticLockingExamples extends HibernateTest {
                     .lock(employee);
 
             log.info("Check logs to see the locking SQL expression.");
+        });
+    }
+
+    @Test
+    void predicateLockingExample() {
+        doInHibernate(session -> {
+            session.createQuery("" +
+                            "SELECT e " +
+                            "FROM Employee e " +
+                            "WHERE e.jobId = 'SA_REP'", Employee.class)
+                    .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+                    .getResultList();
+        });
+    }
+
+    @Test
+    void predicateLockingNoWaitExample() {
+        LockOptions lockOptions = new LockOptions(LockMode.PESSIMISTIC_WRITE);
+        lockOptions.setTimeOut(LockOptions.NO_WAIT);
+
+        doInHibernate(session -> {
+            session.find(Employee.class, 1,
+                    LockModeType.PESSIMISTIC_WRITE,
+                    Collections.singletonMap(
+                            AvailableSettings.JPA_LOCK_TIMEOUT,
+                            LockOptions.NO_WAIT
+                    )
+            );
         });
     }
 
